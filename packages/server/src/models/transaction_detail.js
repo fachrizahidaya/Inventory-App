77ab2e34ef0a731965003069c57d648ae3d1ebd5
fachrizahidaya <fachriza.theblues@gmail.com>
2023-03-06 21:58:21 +0700

    updated

---
+++ packages/server/src/models/transaction_detail.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Transaction_Detail extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Transaction_Detail.belongsTo(models.Transaction)
+      Transaction_Detail.belongsTo(models.Inventory)
+    }
+  }
+  Transaction_Detail.init({
+    qty: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Transaction_Detail',
+  });
+  return Transaction_Detail;
+};
