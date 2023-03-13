2c26ffb1186e1b6bf9d123fb32e7924a6b2f6217
fachrizahidaya <fachriza.theblues@gmail.com>
2023-03-13 21:45:46 +0700

    updated

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -6,6 +6,36 @@ const bearerToken = require("express-bearer-token");
 const app = express();
 const path = require("path");
 const { join } = require("path");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+const fs = require("fs");
+
+module.exports = () => {
+  // Get the root path (assuming your webpack config is in the root of your project!)
+  const currentPath = path.join(__dirname);
+
+  // Create the fallback path (the production .env)
+  const basePath = currentPath + "/.env";
+
+  // We're concatenating the environment name to our filename to specify the correct env file!
+  const envPath = basePath + "." + env.ENVIRONMENT;
+
+  // Check if the file exists, otherwise fall back to the production .env
+  const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+
+  // Set the path parameter in the dotenv config
+  const fileEnv = dotenv.config({ path: finalPath }).parsed;
+
+  // reduce it to a nice object, the same as before (but with the variables from the file)
+  const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+    return prev;
+  }, {});
+
+  return {
+    plugins: [new webpack.DefinePlugin(envKeys)],
+  };
+};
 
 const PORT = process.env.PORT || 8000;
 app.use(express.json());
