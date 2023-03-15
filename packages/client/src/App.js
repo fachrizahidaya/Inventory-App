c5a26d5c32ea1ff30ce76221c79e73449e98e216
fachrizahidaya <fachriza.theblues@gmail.com>
2023-03-15 16:33:23 +0700

    updated

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { LandingUser } from "./user/pages/Landing";
 import { LoginUser } from "./user/pages/Login";
+import NotFound from "./user/pages/NotFound";
 import { RegisterUser } from "./user/pages/Register";
 // import "./App.css";
 
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LandingUser />}></Route>
         <Route path="/user" element={<LoginUser />}></Route>
         <Route path="/register-user" element={<RegisterUser />}></Route>
+        <Route path="/*" element={<NotFound />}></Route>
       </Routes>
     </div>
   );
