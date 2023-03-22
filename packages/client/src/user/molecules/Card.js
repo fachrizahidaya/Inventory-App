d6b74fbb279d0d686fff97c4c13a101ebd607e48
fachrizahidaya <fachriza.theblues@gmail.com>
2023-03-23 04:12:56 +0700

    updated

--- packages/client/src/user/molecules/Card.js
+++ packages/client/src/user/molecules/Card.js
@@ -22,8 +22,8 @@ export const ProductCard = () => {
   const listProduct = async () => {
     try {
       const res = await Axios.get(
-        `http://localhost:8000/product/list`
-        // `${process.env.REACT_APP_API_URL}/product/list`
+        // `http://localhost:8000/product/list`
+        `${process.env.REACT_APP_API_URL}/product/list`
       );
       console.log(res.data);
       setData(res.data);
